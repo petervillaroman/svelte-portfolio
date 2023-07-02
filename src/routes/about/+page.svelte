9edd4545f3991e664881c45db7d4056c16e55e85
Karell Peter <53671374+petervillaroman@users.noreply.github.com>
2023-07-02 15:54:54 -0700

    picocss + programatic routing

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,4 @@
+<a href="/">Home</a>
 <h1>About Me:</h1>
 
 <body>I'm <code>Karell </code> and I'm here to learn Svelte.</body>
---
+++ src/routes/products/+page.svelte
@@ -0,0 +1,18 @@
+
+<a href="/">Back home</a>
+
+<script>
+
+    
+</script>
+<body>
+    <main class="container"><h1> Look at these products </h1>   </main>
+    <div class="grid">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+      </div>
+</body>
+
+
