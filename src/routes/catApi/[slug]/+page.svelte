391f06e45f9ac3201b099aa6dc5171778ae2a2e4
Karell Peter <53671374+petervillaroman@users.noreply.github.com>
2023-07-15 22:57:55 -0700

    created catApi

---
+++ src/routes/catApi/[slug]/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    /** @type {import('./$types').PageData} */
+    export let data;
+</script>
+
+<h1>{data.post.title}</h1>
+<div>{@html data.post.content}</div>
+
+<style>
+    
+</style>
