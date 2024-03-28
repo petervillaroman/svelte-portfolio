ce203bcbc6403818ad47a44c400904e1e303293d
Karell Peter <53671374+petervillaroman@users.noreply.github.com>
2024-03-28 10:12:20 -0700

    adding more photos

--- src/lib/photo-gallery.svelte
+++ src/lib/photo-gallery.svelte
@@ -1,6 +1,6 @@
 <script>
     // Images paths are now direct, assuming they are correctly placed in the `static/images` directory
-    let images = ['../../shotaPortrait.avif', '../../naraLick.avif', '../../vanNeon.avif', '../../sassyGuy.avif', '../../walkingGuy.avif', '../../rosary.avif', '../../maggie.avif', '../../josh.avif'];
+    let images = ['../../shotaPortrait.avif', '../../naraLick.avif', '../../vanNeon.avif', '../../sassyGuy.avif', '../../walkingGuy.avif', '../../rosary.avif', '../../maggie.avif', '../../josh.avif', '../../bikeGuy.avif', '../../bunnyChickens.avif', '../../maggieBunnyIkea.avif', '../../naraUpsidedown.avif', '../../maggieCostco.avif'];
     let zoomedImage = null;
   
     function toggleZoom(image) {
@@ -29,7 +29,7 @@
       </div>
     {/each}
   </div>
-  
+<!--   
   <style>
     .gallery {
       display: flex;
@@ -41,6 +41,48 @@
       width: 100%;
       max-width: 200px; /* Adjust based on your preference */
       height: auto;
+      transition: transform 0.3s ease; /* Smooth transition for zoom effect */
+    }
+    .overlay {
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background-color: rgba(0, 0, 0, 0.8);
+      z-index: 10;
+    }
+    .overlay img {
+      max-width: 80%;
+      max-height: 80%;
+    }
+  </style>
+   -->
+
+   <style>
+    .gallery {
+      display: flex;
+      flex-wrap: wrap;
+      gap: 10px;
+      cursor: pointer; /* Optional: changes the cursor on hover over images */
+    }
+    .photo {
+      background-color: white; /* Adds a white background */
+      border: 2px solid #ccc; /* Adds a border */
+      overflow: hidden; /* Ensures no overflow of content */
+      display: flex; /* Enables flexbox layout */
+      justify-content: center; /* Center images horizontally */
+      align-items: center; /* Center images vertically */
+      flex: 1 0 calc(33.333% - 20px); /* Adjust the flex-basis to control the size, accounting for gap */
+      max-width: 200px; /* Maximum width of a photo */
+    }
+    .photo img {
+      width: 100%; /* Full width of the container */
+      height: 100%; /* Full height of the container */
+      object-fit: cover; /* Cover the container without losing aspect ratio */
       transition: transform 0.3s ease; /* Smooth transition for zoom effect */
     }
     .overlay {
